a0daeeba560ce261f253e89f691c6a4b85b28102
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-10 17:58:07 -0300

    added scaler

--- SVM.py
+++ SVM.py
@@ -10,10 +10,8 @@ import numpy as np
 
 
 from sklearn.model_selection import train_test_split
-
-
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
-
 from sklearn.svm import SVC
 
 data_svm = pd.read_csv(r"C:\Users\nehak\Downloads\UniversalBank.csv")
@@ -25,9 +23,11 @@ data_svm.describe()
 data_x = data_svm.iloc[:,:13]
 data_y = data_svm.iloc[:,-1:]
 
+scaler = StandardScaler()
+scale_x = scaler.fit_transform(data_x)
 
 
-train_x, test_x, train_y, test_y = train_test_split(data_x,data_y,test_size=0.2)
+train_x, test_x, train_y, test_y = train_test_split(scale_x,data_y,test_size=0.2)
 
 model = SVC(kernel='rbf',random_state=5)
 model.fit(train_x, train_y)
