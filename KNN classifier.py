7734b3e7ae045108271af321ded1fd3b7908a690
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-11 14:04:32 -0300

    added scalar

--- KNN classifier.py
+++ KNN classifier.py
@@ -18,10 +18,16 @@ data_y = data.iloc[:,-1:]
 train_x,test_x, train_y, test_y = train_test_split(data_x,data_y,test_size=0.2,)
 
 
+scaler = StandardScaler()
+
+scalar_train_x = scaler.fit_transform(train_x)
+
+scalar_test_x = scaler.fit_transform(test_x)
+
 
 classfier = KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
-classfier.fit(train_x,train_y)
+classfier.fit(scalar_train_x,train_y)
 
-prediction = classfier.predict(test_x)
+prediction = classfier.predict(scalar_test_x)
 
 acc = accuracy_score(test_y,prediction)
