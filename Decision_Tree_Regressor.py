4a4677898db8824206b58a59df58e3199a5a5992
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-12 14:45:03 -0300

    added hyper parameter tunning

--- Decision_Tree_Regressor.py
+++ Decision_Tree_Regressor.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error,mean_absolute_error
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score,GridSearchCV
 
 
 data = pd.read_csv(r"C:\Users\nehak\Downloads\enhanced_tips_dataset.csv")
@@ -50,3 +50,24 @@ DT_MSE = mean_squared_error(test_y, DT_Prediction)
 print('MAE:', mean_absolute_error(test_y, DT_Prediction))
 print('MSE:', mean_squared_error(test_y, DT_Prediction))
 print('RMSE:', np.sqrt(mean_squared_error(test_y, DT_Prediction)))
+
+
+# Hyper parameter tunning 
+
+param_grid = {"splitter":["best", "random"],
+              "max_depth":[2,3,4,5,6,7,8,9,10],
+              "min_samples_leaf":[1,2,3,4,5,6,7,8,9,10],
+              "min_weight_fraction_leaf":[0.1,0.2,0.3,0.4,0.5],
+              "max_features":["auto", "sqrt", "log2",None],
+              "max_leaf_nodes":[None,10,20,30,40,50,60,70,80,90],              
+              }
+
+GV_model = GridSearchCV(DT_model, param_grid,scoring='neg_mean_squared_error',cv=5,verbose=3)
+
+import time
+
+s_time = time.time()
+GV_model.fit(train_x, train_y)
+e_time= time.time()
+
+print("total time :", e_time-s_time)
