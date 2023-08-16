9471874ef18b6b10255d8ee77534ba581ff9e47a
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-15 21:37:34 -0300

    added logistic regression

--- Logistic_Classifier.py
+++ Logistic_Classifier.py
@@ -1,57 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug  9 15:42:08 2023
-
-@author: nehak
-"""
-
-# Linear Regression 
-
-
+import pandas as pd
 import numpy as np
-import pandas as pd 
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import time
+from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_squared_error
+data = pd.read_csv(r"C:\Users\nehak\Downloads\enhanced_tips_dataset.csv")
 
-import plotly.graph_objects as go
-import plotly
-from plotly.offline import download_plotlyjs, init_notebook_mode,  plot
+data.describe()
 
-data_train = pd.read_csv(r"C:\Users\nehak\Downloads\train.csv")
+data.info()
 
-data_test = pd.read_csv(r"C:\Users\nehak\Downloads\test.csv")
+data.isnull().sum()
 
+data = data.drop(['sex','id'],axis=1)
+# num_col = data.select_dtype(['int64'],'float64')
 
-data_train.info()
-data_test.info()
-
-
-data_train.describe()
-data_test.describe()
-
-data_train= data_train.dropna()
-data_test = data_test.dropna()
+data_x = data.loc[:, data.columns != 'sex_binary']
+data_y = data.loc[:, 'sex_binary']
 
 
-X_train = np.array(data_train.iloc[:, :-1].values)
-y_train = np.array(data_train.iloc[:, 1].values)
-X_test = np.array(data_test.iloc[:, :-1].values)
-y_test = np.array(data_test.iloc[:, 1].values)
-model = LinearRegression()
+num_col = data_x.select_dtypes(['int64', 'float64'])
+cat_col = data_x.select_dtypes('object')
+
+encoder = LabelEncoder()
+cat_col = cat_col.apply(encoder.fit_transform)
+
+data_x_updated = pd.DataFrame(pd.concat([num_col, cat_col], axis=1))
+
+train_x, test_x, train_y, test_y = train_test_split(
+    data_x_updated, data_y, test_size=0.2)
 
 
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+# logistic regression 
 
-r2 = r2_score(data_test["y"], y_pred)
-MSE = mean_squared_error(data_test["y"], y_pred)
-
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=data_train["x"],y=data_train["y"],mode="markers"))
-fig.add_trace(go.Scatter(x=data_test["x"],y=y_pred))
-fig.show()
-plot(fig)
-
-
-
+LogR_model = LogisticRegression()
+LogR_model.fit(train_x,train_y)
+LogR_Prediction = LogR_model.predict(test_x)
+LogR_accuracy = accuracy_score(test_y,LogR_Prediction)
