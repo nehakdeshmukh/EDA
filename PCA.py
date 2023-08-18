335d3d94f39927cf95e61289a47736fb90455e38
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-18 15:11:10 -0300

    added pca using sklearn library

--- PCA.py
+++ PCA.py
@@ -10,6 +10,7 @@ import pandas as pd
 from sklearn.datasets import make_blobs
 from sklearn import datasets
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class PCA:
@@ -45,3 +46,28 @@ y = data.target
 pca = PCA(2)
 pca.fit(X)
 X_projected = pca.transform(X)
+
+
+x1 = X_projected[:,0]
+x2 = X_projected[:,1]
+
+plt.scatter(x1,x2,c=y,edgecolor='none',alpha=0.8,cmap=plt.cm.get_cmap('viridis',3))
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.colorbar()
+plt.show()
+
+
+# using module 
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2)
+ 
+X_projected_pca = pca.fit_transform(X)
+
+plt.scatter(X_projected_pca[:,0],X_projected_pca[:,1],c=y,edgecolor='none',alpha=0.8,cmap=plt.cm.get_cmap('viridis',3))
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.colorbar()
+plt.show()
+
+
