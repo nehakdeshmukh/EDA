7b12b3e9ecce0c21ab581e27822d5506ee8a16e1
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-20 19:42:11 -0300

    added DBSCAN clustering

---
+++ DBSCAN_clustering.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 19:20:28 2023
+
+@author: nehak
+"""
+
+# DBSCAN clustering 
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.offline import plot
+from sklearn.cluster import DBSCAN
+
+data_1 = pd.read_csv(r"C:\Users\nehak\Downloads\Blobs.csv")
+
+data_2 = pd.read_csv(r"C:\Users\nehak\Downloads\Varied.csv")
+
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data_1.loc[:,'0'],y=data_1.loc[:,'1'],mode='markers'))
+fig.show()
+plot(fig)
+
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data_2.loc[:,'0'],y=data_2.loc[:,'1'],mode='markers'))
+fig.show()
+plot(fig)
+
