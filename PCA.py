0dc8d5c7b23df183353e2219e2a0fee14c176c4c
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-19 22:21:10 -0300

    added explained variance

--- PCA.py
+++ PCA.py
@@ -43,13 +43,15 @@ data = datasets.load_iris()
 X = data.data
 y = data.target
 
-pca = PCA(2)
-pca.fit(X)
-X_projected = pca.transform(X)
+pca1 = PCA(2)
+pca1.fit(X)
+X_projected = pca1.transform(X)
 
 
 x1 = X_projected[:,0]
 x2 = X_projected[:,1]
+
+print("explained variance: ", pca1.explained_variance_ratio_)
 
 plt.scatter(x1,x2,c=y,edgecolor='none',alpha=0.8,cmap=plt.cm.get_cmap('viridis',3))
 plt.xlabel('Principal Component 1')
@@ -70,4 +72,4 @@ plt.ylabel('Principal Component 2')
 plt.colorbar()
 plt.show()
 
-print("explained variance: " ,pca.explained_variance_ratio_)
+print("explained variance: ", pca.explained_variance_ratio_)
