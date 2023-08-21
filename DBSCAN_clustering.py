5a1193aa0fa12b97a37644a8d5622110888cb39e
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-08-21 15:13:38 -0300

    added plots for possible hyper parameters

--- DBSCAN_clustering.py
+++ DBSCAN_clustering.py
@@ -10,6 +10,8 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.offline import plot
 from sklearn.cluster import DBSCAN
+from sklearn.model_selection import GridSearchCV
+import time 
 
 data_1 = pd.read_csv(r"C:\Users\nehak\Downloads\Blobs.csv")
 
@@ -41,8 +43,8 @@ data_1.loc[:,'cluster'] = dbscan.labels_
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=data_1.loc[:,'0'],y=data_1.loc[:,'1'],
-                         mode='markers',marker_color=data_1['cluster'],
-                         marker=dict(colorscale='Viridis')))
+                          mode='markers',marker_color=data_1['cluster'],
+                          marker=dict(colorscale='Viridis')))
 fig.show()
 plot(fig)
 
@@ -52,11 +54,28 @@ prediction = dbscan.fit_predict(data_2)
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(x=data_2.loc[:,'0'],y=data_2.loc[:,'1'],
-                         mode='markers',marker_color=prediction,
-                         marker=dict(colorscale='Viridis')))
+                          mode='markers',marker_color=prediction,
+                          marker=dict(colorscale='Viridis')))
 
 fig.show()
 plot(fig)
 
-# Plot is not showing all points belongs to same cluster 
-# Model tunning is needed 
+# # Plot is not showing all points belongs to same cluster 
+# # Model tunning is needed 
+
+eps_values = [0.3, 0.5, 1, 1.3, 1.5]
+min_samples_values = [2, 5, 10, 20, 80]
+
+
+
+for i in eps_values:
+    for j in min_samples_values:
+        dbscan = DBSCAN(eps=i, min_samples=j)
+        dbscan.fit(data_2)
+        prediction=dbscan.labels_
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=data_2.loc[:,'0'],y=data_2.loc[:,'1'],
+                                 mode='markers',marker_color=prediction,
+                                 marker=dict(colorscale='Viridis')))
+        fig.update_layout(title ='EPS: {} & min_samples: {}'.format(i,j))
+        plot(fig)
